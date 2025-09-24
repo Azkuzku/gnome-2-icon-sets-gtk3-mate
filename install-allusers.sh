366ec35bf593fbfeb93d2486b6a484314dbe76b5
aake <aake@aukusti.wlan>
2025-09-24 18:36:05 +0300

    added install script to install for all users

---
+++ install-allusers.sh
@@ -0,0 +1,2 @@
+sudo rm -rf usr/share/icons/gnome2-icons-gtk3 usr/share/icons/gnome2-smokeyblue-icons-gtk3 usr/share/icons/gnome2-smokeyred-icons-gtk3 usr/share/icons/gnome2-crux-icons-gtk3
+sudo cp -rf gnome2-icons-gtk3 gnome2-smokeyblue-icons-gtk3 gnome2-smokeyred-icons-gtk3 gnome2-crux-icons-gtk3 -t /usr/share/icons
